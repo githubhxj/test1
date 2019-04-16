65112e05679223999204ef8ab4321ea67524b904
githubhxj <879798084qq.com>
2019-04-16 20:07:16 +0800

    sssssssss

---
+++ src/com/aura/test/TestGithub.java
@@ -0,0 +1,15 @@
+package com.aura.test;
+/*************************************
+*@author  H
+*@date    2019年4月16日下午8:03:35
+*@version  1.0
+*@describe
+*@return
+*
+***************************************/
+public class TestGithub {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
